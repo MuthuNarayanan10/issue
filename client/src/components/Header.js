28bc5fd5d5cf2e70b21e6b69eec7147a025d1ed1
Muthu Natha Narayanan A <41100834+MuthuNarayanan10@users.noreply.github.com>
2021-07-29 12:02:51 +0530

    dropdown changes 1

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -21,138 +21,149 @@ export const isMobileOrWeb = () => {
   }
 };
 export default function Header() {
-  const [toggle, setToggle] = useState(false)
+  const [toggle, setToggle] = useState(false);
   return (
     <div className="NavContainer">
-    <div className="topnav">
-      <ul className="social">
-        <li>
-          <a href="">
-            <img src={twitter} className="logohead" />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/company/develearn-technologies">
-            <img src={linkedin} className="logohead" />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.facebook.com/develearntechnologies">
-            <img src={facebook} className="logohead" />
-          </a>
-        </li>
-        <li>
-          <a href="https://mail.google.com/mail/?view=cm&fs=1&to=contact@develearn.in&su=SUBJECT&body=BODY&bcc=contact@develearn.in">
-            <img src={mail} className="logohead" />
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <img src={call} className="logohead" />
-            <span className="mobile">
-              <span>+91 9967118298</span>
-              <span>Give us a call</span>
-            </span>
-          </a>
-        </li>
-      </ul>
+      <div className="topnav">
+        <ul className="social">
+          <li>
+            <a href="">
+              <img src={twitter} className="logohead" />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/company/develearn-technologies">
+              <img src={linkedin} className="logohead" />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com/develearntechnologies">
+              <img src={facebook} className="logohead" />
+            </a>
+          </li>
+          <li>
+            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=contact@develearn.in&su=SUBJECT&body=BODY&bcc=contact@develearn.in">
+              <img src={mail} className="logohead" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={call} className="logohead" />
+              <span className="mobile">
+                <span>+91 9967118298</span>
+                <span>Give us a call</span>
+              </span>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <Navbar expand="lg" className="navbar p-0">
+        <Container className="container-fluid p-0">
+          <Navbar.Brand href="/" className="nav-link navbar-brand">
+            {" "}
+            <img src={logo} className="log" />
+          </Navbar.Brand>
+          {isMobileOrWeb() && (
+            <>
+              {" "}
+              <div className="hamburger">
+                <Navbar.Toggle
+                  aria-controls="basic-navbar-nav"
+                  className="navbar-toggler"
+                />
+              </div>
+              <div className="mobile-menu">
+                <Navbar.Collapse
+                  id="basic-navbar-nav"
+                  className="collapse navbar-collapse"
+                >
+                  <Nav className="ml-auto">
+                    <ul className="navbar-nav">
+                      <li className="nav-item">
+                        <Link to="/aboutus" className="nav-link">
+                          About
+                        </Link>
+                      </li>
+                      <li className="nav-item drop-down-menu">
+                        <Link to="/course" className="nav-link">
+                          Courses
+                        </Link>
+                        <ul>
+                          <Link to="/course" className="nav-link">
+                            <span className="dropclass">DATASCIENCE</span>
+                          </Link>
+                          <Link to="/Gcourses" className="nav-link">
+                            <span className="dropclass">GRAPHICS</span>
+                          </Link>
+                        </ul>
+                      </li>
+                      <li className="nav-item">
+                        <Link to="/xr-learning" className="nav-link">
+                          xr learning
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link to="/events" className="nav-link">
+                          Events
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link to="/contacts" className="nav-link">
+                          Contact Us
+                        </Link>
+                      </li>
+                    </ul>
+                  </Nav>
+                </Navbar.Collapse>
+              </div>
+            </>
+          )}
+          {!isMobileOrWeb() && (
+            <Navbar.Collapse
+              id="basic-navbar-nav"
+              className="collapse navbar-collapse"
+            >
+              <Nav className="ml-auto">
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <Link to="/aboutus" className="nav-link">
+                      About
+                    </Link>
+                  </li>
+                  <li className="nav-item web-drop-down-menu">
+                    <Link to="/" className="nav-link">
+                      Courses
+                    </Link>
+                    <ul>
+                      <Link to="/course" className="nav-link">
+                        <span className="dropclass">DATASCIENCE</span>
+                      </Link>
+                      <Link to="/Gcourses" className="nav-link">
+                        <span className="dropclass">GRAPHICS</span>
+                      </Link>
+                    </ul>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/xr-learning" className="nav-link">
+                      xr learning
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/events" className="nav-link">
+                      Events
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/contacts" className="nav-link">
+                      Contact Us
+                    </Link>
+                  </li>
+                </ul>
+              </Nav>
+            </Navbar.Collapse>
+          )}{" "}
+        </Container>
+      </Navbar>
     </div>
-    <Navbar expand="lg" className="navbar p-0">
-      <Container className="container-fluid p-0">
-        <Navbar.Brand href="/" className="nav-link navbar-brand">
-          {" "}
-          <img src={logo} className="log" />
-        </Navbar.Brand>
-  
-        {isMobileOrWeb() &&      
-        <>    <div className="hamburger">     
-     <Navbar.Toggle
-          aria-controls="basic-navbar-nav"
-          className="navbar-toggler"
-        />
-      </div>
-        <div className="mobile-menu">
-      <Navbar.Collapse
-          id="basic-navbar-nav"
-          className="collapse navbar-collapse"
-        >
-          <Nav className="ml-auto">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link to="/aboutus" className="nav-link">
-                  About
-                </Link>
-              </li>
-              <li className="nav-item drop-down-menu">
-                <Link to="/course" className="nav-link">
-                  Courses
-                </Link>
-                <ul>
-                  <li>1</li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <Link to="/xr-learning" className="nav-link">
-                  xr learning
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/events" className="nav-link">
-                  Events
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/contacts" className="nav-link">
-                  Contact Us
-                </Link>
-              </li>
-            </ul>
-          </Nav>
-        </Navbar.Collapse>
-     
-      </div>
-      </>}
-  
-    {!isMobileOrWeb() &&    <Navbar.Collapse
-          id="basic-navbar-nav"
-          className="collapse navbar-collapse"
-        >
-          <Nav className="ml-auto">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link to="/aboutus" className="nav-link">
-                  About
-                </Link>
-              </li>
-              <li className="nav-item web-drop-down-menu">
-                <Link to="/course" className="nav-link">
-                  Courses
-                </Link>
-                <ul>
-                  <li>123</li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <Link to="/xr-learning" className="nav-link">
-                  xr learning
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/events" className="nav-link">
-                  Events
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/contacts" className="nav-link">
-                  Contact Us
-                </Link>
-              </li>
-            </ul>
-          </Nav>
-        </Navbar.Collapse>
-     } </Container>
-    </Navbar>
-  </div>
-
-  )
+  );
 }
